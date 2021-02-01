a1511a71315c444ceb13db2a4ffad31c6fc73b87
chil13 <matsuo.mbc6@gmail.com>
2021-02-01 23:50:26 +0900

    あああ

--- test5/src/servlet/IndexServlet.java
+++ test5/src/servlet/IndexServlet.java
@@ -21,6 +21,7 @@ public class IndexServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
